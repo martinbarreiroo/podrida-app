377e63570302f747189baf1c9192d93bea69b062
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2025-01-21 20:08:23 -0300

    removed images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,13 @@
 
 import styles from './Header.module.scss';
 import ResetButton from "@/components/ResetButton/ResetButton";
-import Image from "next/image";
+
 
 function Header({ playerCount, onIncrement, onDecrement, onReset }) {
     return (
         <div className={styles.header}>
             <div className={styles.titleContainer}>
-                <Image src="/podrida.png" alt="Podrida" width={50} height={50} />
                 <h1>Podrida</h1>
-                <Image src="/podrida.png" alt="Podrida" width={50} height={50} />
             </div>
             <div className={styles.controls}>
                 <span className={styles.span}>Number of Players: {playerCount}</span>
