550686b0940d067d97b00c63a32bb7c5172b6424
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2025-01-21 20:17:34 -0300

    images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,15 @@
 
 import styles from './Header.module.scss';
 import ResetButton from "@/components/ResetButton/ResetButton";
-
+import Image from "next/image";
 
 function Header({ playerCount, onIncrement, onDecrement, onReset }) {
     return (
         <div className={styles.header}>
             <div className={styles.titleContainer}>
+                <Image src="/assets/podrida.png" alt="Podrida" width={50} height={50} />
                 <h1>Podrida</h1>
+                <Image src="/assets/podrida.png" alt="Podrida" width={50} height={50} />
             </div>
             <div className={styles.controls}>
                 <span className={styles.span}>Number of Players: {playerCount}</span>
