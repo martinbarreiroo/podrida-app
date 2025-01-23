f3aec3ac5306373bfd076ef3d2b2d879612476f0
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2025-01-23 00:49:15 -0300

    Added gifs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,19 @@ function Header({ playerCount, onIncrement, onDecrement, onReset }) {
   return (
     <div className={styles.header}>
       <div className={styles.titleContainer}>
-        <Image src="/assets/podrida.png" alt="Podrida" width={50} height={50} />
+        <Image
+          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExazcwbzN6c3hvNzB5ODB2NjFxeXhzcThzZ2QwYmd5NjlkY3B0dzlzbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oz8xJEqVd9MQ1enBe/giphy.gif"
+          alt="Podrida"
+          width={75}
+          height={75}
+        />
         <h1>Podrida</h1>
-        <Image src="/assets/podrida.png" alt="Podrida" width={50} height={50} />
+        <Image
+          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExazcwbzN6c3hvNzB5ODB2NjFxeXhzcThzZ2QwYmd5NjlkY3B0dzlzbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oz8xJEqVd9MQ1enBe/giphy.gif"
+          alt="Podrida"
+          width={75}
+          height={75}
+        />
       </div>
       <div className={styles.controls}>
         <span className={styles.span}>Number of Players: {playerCount}</span>
