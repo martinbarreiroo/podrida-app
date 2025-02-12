b8005c2a9784433fe773c3322e9942eea1ba2a9c
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2025-02-12 19:51:26 -0300

    gifs are now locally retrieved

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header({ playerCount, onIncrement, onDecrement, onReset }) {
     <div className={styles.header}>
       <div className={styles.titleContainer}>
         <Image
-          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExazcwbzN6c3hvNzB5ODB2NjFxeXhzcThzZ2QwYmd5NjlkY3B0dzlzbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oz8xJEqVd9MQ1enBe/giphy.gif"
+          src="/assets/podrida.gif"
           alt="Podrida"
           width={75}
           height={75}
@@ -17,7 +17,7 @@ function Header({ playerCount, onIncrement, onDecrement, onReset }) {
         />
         <h1>Podrida</h1>
         <Image
-          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExazcwbzN6c3hvNzB5ODB2NjFxeXhzcThzZ2QwYmd5NjlkY3B0dzlzbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oz8xJEqVd9MQ1enBe/giphy.gif"
+          src="/assets/podrida.gif"
           alt="Podrida"
           width={75}
           height={75}
