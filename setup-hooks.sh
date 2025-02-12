537c6a438d62d1b0e7542bedef01d0a0b713f090
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2025-02-12 20:17:02 -0300

    git config for git hooks

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -2,6 +2,8 @@
 
 echo "Configuring permissions for Git hooks..."
 
+git config core.hooksPath .scripts
+
 # Set executable permissions for pre-commit hook
 chmod +x .scripts/pre-commit
 # Add other hooks here ...
